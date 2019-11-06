6f0f758be693294f8b001f378f7206fd83328056
zluj <989239892@qq.com>
2019-11-06 17:36:31 +0800

    取account

--- Inspection/app/src/main/java/com/demo/inspection/ui/Fragment/MeFragment.java
+++ Inspection/app/src/main/java/com/demo/inspection/ui/Fragment/MeFragment.java
@@ -1,3 +1,8 @@
+/*
+ * 描述:
+ *   实现我的页面基本功能
+ *   2019-11
+ */
 package com.demo.inspection.ui.Fragment;
 
 import android.content.Intent;
@@ -8,71 +13,55 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import com.demo.inspection.R;
 import com.demo.inspection.bl.ComDef;
 import com.demo.inspection.bl.GetData;
 import com.demo.inspection.bl.ReqParam;
 import com.demo.inspection.ui.Fragment.barchart.XgPwd;
-import com.demo.inspection.utils.ScreenUtil;
-import com.demo.inspection.utils.ToastUtil;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
 public class MeFragment extends Fragment implements View.OnClickListener {
     View view;
     static Bundle bundle = new Bundle();
     String dept, account, telphone, username;
-
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-//        ScreenUtil screenUtil = new ScreenUtil();
-//        int dpi = screenUtil.getScreenDPI(this);
-//        screenUtil.adapterScreen(this, dpi, false);
-
         view = inflater.inflate(R.layout.activity_me, container, false);
-
+        //跳转修改密码页面
         Button button = view.findViewById(R.id.xg_m);
         button.setOnClickListener((view) -> {
             Intent intent = new Intent(getActivity(), XgPwd.class);
             startActivity(intent);
-
-
         });
-        TextView txtName = view.findViewById(R.id.uid);
+
         TextView name = view.findViewById(R.id.uname);
         TextView uid = view.findViewById(R.id.uid);
         TextView phone = view.findViewById(R.id.uphone);
         TextView depart = view.findViewById(R.id.udepart);
         bundle = getArguments();
-
+        // 与接口交互
         ReqParam req = new ReqParam();
-        req.setUrl(ComDef.INTF_QUERYADMIN);//修改为实际接口
+        req.setUrl(ComDef.INTF_QUERYADMIN);
         HashMap map = new HashMap<String, String>();
-        map.put(ComDef.QUERY_ACCOUNT, bundle.getString("username"));//修改为实际请求参数
+        map.put(ComDef.QUERY_ACCOUNT, bundle.getString("username"));
         req.setMap(map);
 
-        Log.i(ComDef.TAG, "我的开始查询");
+       //  Log.i(ComDef.TAG, "我的开始查询");
         new GetData(req) {
             @Override
             public void dealResult(String result) throws JSONException {
-
-                List<Map<String, String>> list = new ArrayList<>();
                 JSONArray array = new JSONArray(result);
                 JSONObject item = (JSONObject) array.get(0);
+                // 获取到需要数据
                 dept = item.getString("dept");
                 account = item.getString("account");
                 username = item.getString("username");
@@ -83,62 +72,24 @@ public class MeFragment extends Fragment implements View.OnClickListener {
                     uid.setText(account);
                     phone.setText(telphone);
                     depart.setText(dept);
-
-
                 });
-
             }
-
-
         };
-
-
-//        bundle=getArguments();
-//        map.put("id", "00001");//2
-//        map.put("account", "admin");
-//        map.put("username", bundle.getString("username"));//1
-//        map.put("phone", "13666666666");//3
-//        map.put("dept", "信息化");//4
-//        map.put("password", "000000");//5
-//        list.add(map);
-
-
-//        Log.i("XX", list.get(0).get("username"));
-
-//        String id = list.get(0).get("id");
-//        String username = list.get(0).get("username");
-//        String phones = list.get(0).get("phone");
-//        String dept = list.get(0).get("dept");
-//        String account = list.get(0).get("account");
-
-
         return view;
     }
-
-
-    public static MeFragment getInstances(String name) {
-        MeFragment meFragment = new MeFragment();
-
-        bundle.putString("name", name);
-
-        meFragment.setArguments(bundle);
-        return meFragment;
-    }
-
-
+             public static MeFragment getInstances(String name) {
+                 MeFragment meFragment = new MeFragment();
+                 bundle.putString("name", name);
+                 meFragment.setArguments(bundle);
+             return meFragment;
+             }
     @Override
     public void onClick(View view) {
-
     }
-
     public static MeFragment getUserName(String username) {
         MeFragment meFragment = new MeFragment();
-
         bundle.putString("username", username);
-
         meFragment.setArguments(bundle);
         return meFragment;
-
     }
-
 }
--- Inspection/app/src/main/java/com/demo/inspection/ui/Fragment/barchart/XgPwd.java
+++ Inspection/app/src/main/java/com/demo/inspection/ui/Fragment/barchart/XgPwd.java
@@ -1,10 +1,14 @@
+/*
+ * 描述:
+ *   实现密码修改基本功能
+ *   2019-11
+ */
 package com.demo.inspection.ui.Fragment.barchart;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
-
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,42 +16,23 @@ import com.demo.inspection.R;
 import com.demo.inspection.bl.ComDef;
 import com.demo.inspection.bl.GetData;
 import com.demo.inspection.bl.ReqParam;
-import com.demo.inspection.ui.LoginActivity;
-import com.demo.inspection.utils.ScreenUtil;
 import com.demo.inspection.utils.ToastUtil;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class XgPwd extends AppCompatActivity {
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-
-//
-//        ScreenUtil screenUtil=new ScreenUtil();
-//        int dpi=screenUtil.getScreenDPI(this);
-//        screenUtil.adapterScreen(this,dpi,false);
-
         setContentView (R.layout.activity_xgpwd);
-
         ActionBar actionBar = getSupportActionBar();
         // 显示返回按钮
         actionBar.setDisplayHomeAsUpEnabled(true);
         // 去掉logo图标
         actionBar.setDisplayShowHomeEnabled(false);
         actionBar.setTitle("返回");
-
-
         Bundle bundle=new Bundle();
         TextView xgpwd_edit_name = findViewById(R.id.xgpwd_edit_name);
         TextView xgpwd_edit_old = findViewById(R.id.xgpwd_edit_old);
@@ -55,22 +40,18 @@ public class XgPwd extends AppCompatActivity {
         TextView xgpwd_edit_check = findViewById(R.id.xgpwd_edit_check);
         Button xgpwd_sure = findViewById(R.id.xgpwd_sure);
         Button xgpwd_cancel = findViewById(R.id.xgpwd_cancel);
-
-
-
+        //验证输入信息是否正确
         xgpwd_sure.setOnClickListener(v -> {
             if (xgpwd_edit_name.getText().toString().equals("admin")) {
                 if (xgpwd_edit_check.getText().toString().equals(xgpwd_edit_new.getText().toString())) {
-                    ReqParam req = new ReqParam();
-                    HashMap map = new HashMap<String, String>();
-                    map.put(ComDef.QUERY_ACCOUNT, "admin");
-                    req.setUrl(ComDef.INTF_QUERYADMIN);
-                    req.setMap(map);
+                        ReqParam req = new ReqParam();
+                        HashMap map = new HashMap<String, String>();
+                        map.put(ComDef.QUERY_ACCOUNT, "admin");
+                        req.setUrl(ComDef.INTF_QUERYADMIN);
+                        req.setMap(map);
 
                     new GetData(req) {
                         public void dealResult(String result) throws JSONException {
-//                            ReqParam  req1 = new ReqParam();
-//                            HashMap map1 = new HashMap<String, String>();
                             JSONArray array = new JSONArray(result);
                             JSONObject item = (JSONObject) array.get(0);
                              String dbpasswd= item.getString("password");
@@ -91,44 +72,17 @@ public class XgPwd extends AppCompatActivity {
                 }
             } else{
                 ToastUtil.toastCenter(this, "用户不存在！");
-        }
+            }
         });
-
-//        bundle.getString("username");
-
         xgpwd_cancel.setOnClickListener(v -> {
             this.finish();
-        });
-
+            });
         };
-
-
-//        bundle=getArguments();
-//        map.put("id", "00001");//2
-//        map.put("account", "admin");
-//        map.put("username", bundle.getString("username"));//1
-//        map.put("phone", "13666666666");//3
-//        map.put("dept", "信息化");//4
-//        map.put("password", "000000");//5
-//        list.add(map);
-
-
-//        Log.i("XX", list.get(0).get("username"));
-
-//        String id = list.get(0).get("id");
-//        String username = list.get(0).get("username");
-//        String phones = list.get(0).get("phone");
-//        String dept = list.get(0).get("dept");
-//        String account = list.get(0).get("account");
-
     private void Update_Password( String password) {
         ReqParam req1 = new ReqParam();
         req1.setUrl(ComDef.INTF_UPDATEADMIN);//修改为实际接口
         HashMap map1 = new HashMap<String, String>();
-//        map.put(ComDef.QUERY_DATE, "2019-11-02");//修改为实际请求参数
-//        map.put(ComDef.QUERY_INDEX, "1");//修改为实际请求参数
         map1.put(ComDef.QUERY_ACCOUNT, "admin");
-
         map1.put(ComDef.MODIFY_PW, password);//修改为实际请求参数
         req1.setMap(map1);
 //                xgpwd_edit_check.getText().toString()
@@ -139,10 +93,8 @@ public class XgPwd extends AppCompatActivity {
                     ToastUtil.toastCenter(XgPwd.this, result);
                 });
             }
-
         };
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -151,11 +103,8 @@ public class XgPwd extends AppCompatActivity {
                 return false;
             default:
                 return super.onOptionsItemSelected(item);
+            }
         }
-    }
-
-
-
     }
 
 
