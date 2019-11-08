e652843746d5027b85f9f5902c42c7d7c6dfd512
admin <79032937@qq.com>
2019-11-08 15:04:48 +0800

    代码整理1

--- Inspection/app/src/main/java/com/demo/inspection/ui/MainActivity.java
+++ Inspection/app/src/main/java/com/demo/inspection/ui/MainActivity.java
@@ -39,63 +39,57 @@ import java.util.HashMap;
 import static com.demo.inspection.utils.ComDef.TITLE_NAME;
 
 public class MainActivity extends BaseActivity {
+
     BottomNavigationView mainBottom;
     ViewPager mainViewpage;
     String username;
     // 定义一个变量，来标识是否退出
     private boolean isExit = false;
-
     private ArrayList<Fragment> fList;
     private TextView mTv;
     private static final int NETWORK_ID = Menu.FIRST;
     private static final int Exit_ID = Menu.FIRST + 1;
-    Bundle bundle = new Bundle();
+    Bundle bundle = new Bundle ();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
+        super.onCreate (savedInstanceState);
 
-        setContentView(R.layout.activity_main);
-        setTitle(TITLE_NAME[0]);
+        setContentView (R.layout.activity_main);
+        setTitle (TITLE_NAME[0]);
 
-        Intent intent = getIntent();
+        Intent intent = getIntent ();
         // 获得登录页面的bundle(用户名)
-        bundle = intent.getExtras();
-        username = bundle.getString("username");
+        bundle = intent.getExtras ();
+        username = bundle.getString ("username");
 
+        mainViewpage = findViewById (R.id.main_viewpage);//ViewPager
+        mainBottom = findViewById (R.id.main_bottom);//底部导航栏
 
+        userFragmentPagerAdapter ();
 
+        mainBottom.setOnNavigationItemSelectedListener (menuItem -> {
 
-        /*
-         * 底部导航菜单栏
-         * */
-        mainViewpage = findViewById(R.id.main_viewpage);//ViewPager
-        mainBottom = findViewById(R.id.main_bottom);//底部导航栏
+            switch (menuItem.getItemId ()) {
 
-        userFragmentPagerAdapter();
+                case R.id.main_state:
+                    mainViewpage.setCurrentItem (0);
+                    break;
+                case R.id.main_dev:
+                    mainViewpage.setCurrentItem (1);
+                    break;
+                case R.id.main_sys:
+                    mainViewpage.setCurrentItem (2);
+                    break;
+                case R.id.main_my:
+                    mainViewpage.setCurrentItem (3);
+                    break;
 
-        mainBottom.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()) {
-                    case R.id.main_state:
-                        mainViewpage.setCurrentItem(0);
-                        break;
-                    case R.id.main_dev:
-                        mainViewpage.setCurrentItem(1);
-                        break;
-                    case R.id.main_sys:
-                        mainViewpage.setCurrentItem(2);
-                        break;
-                    case R.id.main_my:
-                        mainViewpage.setCurrentItem(3);
-                        break;
-                }
-                return true;
             }
+
+            return true;
         });
 
 
@@ -104,40 +98,42 @@ public class MainActivity extends BaseActivity {
 
     public void userFragmentPagerAdapter() {
 
-
-        fList = new ArrayList<>();
-        fList.add(StatusFragment.getInstances("状态"));
-        fList.add(EquipmentFragment.getInstances("设备"));
-        fList.add(SystemFragment.getInstances("系统"));
-        fList.add(MeFragment.getInstances("我的"));
+        fList = new ArrayList<> ();
+        fList.add (StatusFragment.getInstances ("状态"));
+        fList.add (EquipmentFragment.getInstances ("设备"));
+        fList.add (SystemFragment.getInstances ("系统"));
+        fList.add (MeFragment.getInstances ("我的"));
         // 把username 传给我的页面
-        MeFragment.getUserName(username);
+        MeFragment.getUserName (username);
 
-        MyFragmentPagerAdapter adapter = new MyFragmentPagerAdapter(fList, getSupportFragmentManager());
-        mainViewpage.setAdapter(adapter);
-        mainViewpage.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+        MyFragmentPagerAdapter adapter = new MyFragmentPagerAdapter (fList, getSupportFragmentManager ());
+        mainViewpage.setAdapter (adapter);
+        mainViewpage.addOnPageChangeListener (new ViewPager.OnPageChangeListener () {
+
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
             }
 
             @Override
             public void onPageSelected(int position) {
+
                 switch (position) {
+
                     case 0:
-                        mainBottom.setSelectedItemId(R.id.main_state);
-                        setTitle(TITLE_NAME[0]);
+                        mainBottom.setSelectedItemId (R.id.main_state);
+                        setTitle (TITLE_NAME[0]);
                         break;
                     case 1:
-                        mainBottom.setSelectedItemId(R.id.main_dev);
-                        setTitle(TITLE_NAME[1]);
+                        mainBottom.setSelectedItemId (R.id.main_dev);
+                        setTitle (TITLE_NAME[1]);
                         break;
                     case 2:
-                        mainBottom.setSelectedItemId(R.id.main_sys);
-                        setTitle(TITLE_NAME[2]);
+                        mainBottom.setSelectedItemId (R.id.main_sys);
+                        setTitle (TITLE_NAME[2]);
                         break;
                     case 3:
-                        mainBottom.setSelectedItemId(R.id.main_my);
-                        setTitle(TITLE_NAME[3]);
+                        mainBottom.setSelectedItemId (R.id.main_my);
+                        setTitle (TITLE_NAME[3]);
                         break;
                 }
             }
@@ -148,33 +144,31 @@ public class MainActivity extends BaseActivity {
         });
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
-        menu.add(0, NETWORK_ID, 0, R.string.network);
-        menu.add(0, Exit_ID, 0, R.string.exit);
+        menu.add (0, NETWORK_ID, 0, R.string.network);
+        menu.add (0, Exit_ID, 0, R.string.exit);
 
-        return super.onCreateOptionsMenu(menu);
+        return super.onCreateOptionsMenu (menu);
     }
-
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        switch (item.getItemId()) {
+        switch (item.getItemId ()) {
 
             case NETWORK_ID:
-                Intent netWork = new Intent(this, NetWorkActivity.class);
-                startActivity(netWork);
+                Intent netWork = new Intent (this, NetWorkActivity.class);
+                startActivity (netWork);
 
                 break;
             case Exit_ID:
-                System.exit(0);
+                System.exit (0);
 
         }
 
-        return super.onOptionsItemSelected(item);
+        return super.onOptionsItemSelected (item);
     }
 
     /**
@@ -184,13 +178,15 @@ public class MainActivity extends BaseActivity {
     public boolean onKeyDown(int keyCode, KeyEvent event) {
 
         if (keyCode == KeyEvent.KEYCODE_BACK) {
+
             if (isExit) {
-                this.finish();
+                this.finish ();
 
             } else {
-                Toast.makeText(this, "再按一次退出", Toast.LENGTH_SHORT).show();
+
+                Toast.makeText (this, "再按一次退出", Toast.LENGTH_SHORT).show ();
                 isExit = true;
-                new Handler().postDelayed(new Runnable() {
+                new Handler ().postDelayed (new Runnable () {
                     @Override
                     public void run() {
                         isExit = false;
@@ -200,80 +196,64 @@ public class MainActivity extends BaseActivity {
             return true;
         }
 
-        return super.onKeyDown(keyCode, event);
+        return super.onKeyDown (keyCode, event);
     }
 
     public void imageViewonClick(View v) {
 
-        Toast.makeText(this, "你已经点击了图片", Toast.LENGTH_SHORT).show();
-//        Intent netWork1=new Intent(this, SystemListActivity.class);
-//        startActivity(netWork1);
-        new AlertDialog.Builder(this)
-                .setTitle("系统")
-                .setItems(
+        Toast.makeText (this, "你已经点击了图片", Toast.LENGTH_SHORT).show ();
+
+        new AlertDialog.Builder (this)
+                .setTitle ("系统")
+                .setItems (
                         R.array.Pos_items,
                         (dialog, which) -> {
                             if (which == 0) {//新增
 
-                                Intent intent = new Intent(this, SystemAddActivity.class); //参数1:Fragment所依存的Activity,参数2：要跳转的Activity
-
-//
-//                                Bundle bundle = new Bundle();
-//                                TextView one = findViewById(R.id.textViewSBid);
-//                                String id = one.getText().toString();
-//                                bundle.putString("id", id);
-//                                intent.putExtras(bundle);
-                                startActivity(intent); //这里一定要获取到所在Activity再startActivity()；
-
+                                Intent intent = new Intent (this, SystemAddActivity.class); //参数1:Fragment所依存的Activity,参数2：要跳转的Activity
+                                startActivity (intent); //这里一定要获取到所在Activity再startActivity()；
 
                             } else if (which == 1) {// 修改
-                                Intent intent1 = new Intent(this, SystemModActivity.class); //参数1:Fragment所依存的Activity,参数2：要跳转的Activity
 
+                                Intent intent1 = new Intent (this, SystemModActivity.class); //参数1:Fragment所依存的Activity,参数2：要跳转的Activity
+                                Bundle bundle = new Bundle ();
+                                TextView one = findViewById (R.id.textViewSBid);
+                                String id = one.getText ().toString ();
 
-                                Bundle bundle = new Bundle();
-                                TextView one = findViewById(R.id.textViewSBid);
-                                String id = one.getText().toString();
+                                bundle.putString ("id", id);
 
-                                System.out.println(" bundle.putString"+id);
-
-
-                                bundle.putString("id", id);
-
-                                intent1.putExtras(bundle);
-                                startActivity(intent1); //这里一定要获取到所在Activity再startActivity()；
-
+                                intent1.putExtras (bundle);
+                                startActivity (intent1); //这里一定要获取到所在Activity再startActivity()；
 
                             } else {  //删除
 
-                                ReqParam req = new ReqParam();
-                                req.setUrl(ComDef.INTF_SYSDEL);//从INTF_SYSDEL接口删除数据
-                                HashMap map1 = new HashMap<String, String>();
+                                ReqParam req = new ReqParam ();
+                                req.setUrl (ComDef.INTF_SYSDEL);//从INTF_SYSDEL接口删除数据
+                                HashMap map1 = new HashMap<String, String> ();
 
-                                TextView one = findViewById(R.id.textViewSBid);
-                                String id1 = one.getText().toString();
+                                TextView one = findViewById (R.id.textViewSBid);
+                                String id1 = one.getText ().toString ();
 
+                                map1.put (ComDef.QUERY_SYSINDEX, id1); //获取需要的字段：sysName, editTextXTMCInput.getText().toString()); //获取需要的字段：sysName
 
-                                map1.put(ComDef.QUERY_SYSINDEX, id1); //获取需要的字段：sysName, editTextXTMCInput.getText().toString()); //获取需要的字段：sysName
+                                req.setMap (map1);
 
-                                req.setMap(map1);
+                                new GetData (req) {
 
-                                new GetData(req) {
                                     @Override
                                     public void dealResult(String result) throws JSONException {
-                                        MainActivity.this.runOnUiThread(() -> {
-                                            ToastUtil.toastCenter(MainActivity.this, result);
+
+                                        MainActivity.this.runOnUiThread (() -> {
+                                            ToastUtil.toastCenter (MainActivity.this, result);
                                         });
                                     }
 
                                 };
 
-
                             }
-                        }).show();
-
+                        }).show ();
 
     }
-
 
 }
 
