1e37a444949f2a4b2decf3124184895be9e01b12
zhangwenpu-1 <1547160085@qq.com>
2019-11-09 10:52:58 +0800

    最终版

--- Inspection/app/src/main/java/com/demo/inspection/SystemModActivity.java
+++ Inspection/app/src/main/java/com/demo/inspection/SystemModActivity.java
@@ -19,20 +19,15 @@ import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.demo.inspection.bl.GetData;
 import com.demo.inspection.bl.ReqParam;
 import com.demo.inspection.utils.ComDef;
 import com.demo.inspection.utils.ToastUtil;
-import com.demo.inspection.utils.Tools;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.HashMap;
 
 public class SystemModActivity extends AppCompatActivity {
@@ -41,8 +36,8 @@ public class SystemModActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_systemmod);
-        ActionBar actionBar = getSupportActionBar();
 
+        ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
@@ -52,80 +47,87 @@ public class SystemModActivity extends AppCompatActivity {
 
         /**
          *
-         *系统详情数据查询
+         *系统列表详情数据查询
          */
 
-        TextView js = findViewById(R.id.editTextXTMC1);
-        TextView uid = findViewById(R.id.editTextXTXQ1);
-        TextView ot = findViewById(R.id.editTextOT1);
-        TextView userid = findViewById(R.id.editTextUserid1);
+        TextView name = findViewById(R.id.editTextSysName);
+        TextView detial = findViewById(R.id.editTextSysDetial);
+        TextView linkMan = findViewById(R.id.editTextSysLinkMan);
+        TextView linkManPhone = findViewById(R.id.editTextSysLinkManPhone);
 
         ReqParam req = new ReqParam();
         HashMap map = new HashMap<String, String>();
-        map.put(ComDef.QUERY_SYSINDEX,bundle.getString("id"));//用查询索引值作为条件
-        Log.i(ComDef.TAG, "查询条件-------------------------------------------:" + bundle.getString("id"));
+        map.put(ComDef.QUERY_SYSINDEX, id);//用查询索引值作为条件
+        Log.i(ComDef.TAG, "查询条件-------------------------------------------:" + id);
         req.setMap(map);
-        req.setUrl(ComDef.INTF_QUERYSYSDETAIL );//从查询系统详情查询数据
+        req.setUrl(ComDef.INTF_QUERYSYSDETAIL);//从查询系统详情查询数据
+
         new GetData(req) {
+
             @Override
             public void dealResult(String result) throws JSONException {
+
                 JSONArray array = new JSONArray(result);
                 for (int i = 0; i < array.length(); i++) {
                     JSONObject item = (JSONObject) array.get(i);
-                    //将查询到的数据写入页面控件
 
-                    js.setText(item.getString("sysName"));
-                    uid.setText(item.getString("detial"));
-                    String result1  = Tools.myDateFormat(item.getString("opttime"));
-                    ot.setText (item.getString("linkman"));
-                    userid.setText(item.getString("phone"));
+                    //将查询到的数据写入页面控件
+                    name.setText(item.getString("sysName"));
+                    detial.setText(item.getString("detial"));
+                    linkMan.setText(item.getString("linkman"));
+                    linkManPhone.setText(item.getString("phone"));
 
                 }
             }
         };
 
-        Button buttonok = findViewById(R.id.buttonOK);
-        buttonok.setOnClickListener((view) -> {
+        Button buttonOk = findViewById(R.id.buttonOK);
+        buttonOk.setOnClickListener((view) -> {
+
             ReqParam req1 = new ReqParam();
             req1.setUrl(ComDef.INTF_SYSMOD);//从INTF_SYSMOD接口修改数据
             HashMap map1 = new HashMap<String, String>();
 
-            map1.put(ComDef.QUERY_SYSINDEX,bundle.getString("id")); //获取需要的字段：sysName, editTextXTMCInput.getText().toString()); //获取需要的字段：sysName
+            //从QUERY_SYSINDEX接口传入修改数据的id值
+            map1.put(ComDef.QUERY_SYSINDEX, id);
+            //通过SYS_NAME接口修改sysName数据
+            EditText editTextSysName = findViewById(R.id.editTextSysName);
+            map1.put(ComDef.SYS_NAME, editTextSysName.getText().toString());
+            Log.i("cccccccccccc", editTextSysName.getText().toString());
 
-            EditText editTextXTMCInput = findViewById(R.id.editTextXTMC1);
-            map1.put(ComDef.SYS_NAME, editTextXTMCInput.getText().toString()); //获取需要的字段：sysName
-            Log.i("cccccccccccc", editTextXTMCInput.getText().toString());
+            //通过SYS_NAME接口修改detial数据
+            EditText editTextSysDetial = findViewById(R.id.editTextSysDetial);
+            map1.put(ComDef.SYS_DETIAL, editTextSysDetial.getText().toString());
 
-            EditText editTextXTXQInput = findViewById(R.id.editTextXTXQ1);
-            map1.put(ComDef.SYS_DETIAL, editTextXTXQInput.getText().toString());//获取需要的字段：detial
+            //通过SYS_NAME接口修改linkman数据
+            EditText editTextSysLinkMan = findViewById(R.id.editTextSysLinkMan);
+            map1.put(ComDef.SYS_LINKMAN, editTextSysLinkMan.getText().toString());
 
-            EditText editTextOTInput = findViewById(R.id.editTextOT1);
-            map1.put(ComDef.SYS_LINKMAN, editTextOTInput.getText().toString());//获取需要的字段联系人
-
-
-           EditText editTextUseridInput = findViewById(R.id.editTextUserid1);
-            map1.put(ComDef.SYS_PHONE, editTextUseridInput.getText().toString());//获取需要的字段联系人电话
-
-            Log.i("cccccccccccc15646984123",editTextUseridInput.getText().toString());
-
+            //通过SYS_NAME接口修改linkmanphone数据
+            EditText editTextSysLinkManPhone = findViewById(R.id.editTextSysLinkManPhone);
+            map1.put(ComDef.SYS_PHONE, editTextSysLinkManPhone.getText().toString());//获取需要的字段联系人电话
+            Log.i("cccccccccccc15646984123", editTextSysLinkManPhone.getText().toString());
             req1.setMap(map1);
 
             new GetData(req1) {
+
                 @Override
                 public void dealResult(String result) throws JSONException {
+
                     SystemModActivity.this.runOnUiThread(() -> {
                         ToastUtil.toastCenter(SystemModActivity.this, result);
                     });
+
                 }
             };
 
-            finish ();
-
+            finish();
         });
 
         Button buttonCancel = findViewById(R.id.buttonCancel);
         buttonCancel.setOnClickListener((view) -> {
-            finish ();
+
+            finish();
 
         });
 
