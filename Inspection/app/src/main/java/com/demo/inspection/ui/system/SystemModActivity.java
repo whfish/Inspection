452135b85777c1a889014d723c448a172f8e6f63
zhangwenpu-1 <1547160085@qq.com>
2019-11-11 10:37:08 +0800

    2019/11/11 10:36修改

--- Inspection/app/src/main/java/com/demo/inspection/ui/system/SystemModActivity.java
+++ Inspection/app/src/main/java/com/demo/inspection/ui/system/SystemModActivity.java
@@ -16,24 +16,22 @@ package com.demo.inspection.ui.system;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.demo.inspection.R;
 import com.demo.inspection.bl.GetData;
 import com.demo.inspection.bl.ReqParam;
 import com.demo.inspection.utils.ComDef;
 import com.demo.inspection.utils.ToastUtil;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -66,7 +64,7 @@ public class SystemModActivity extends AppCompatActivity {
         ReqParam req = new ReqParam();
         HashMap map = new HashMap<String, String>();
         map.put(ComDef.QUERY_SYSINDEX, id);//用查询索引值作为条件
-        Log.i(ComDef.TAG, "查询条件-------------------------------------------:" + id);
+        Log.i(ComDef.TAG, "查询条件------------------:" + id);
         req.setMap(map);
         req.setUrl(ComDef.INTF_QUERYSYSDETAIL);//从查询系统详情查询数据
 
@@ -89,8 +87,6 @@ public class SystemModActivity extends AppCompatActivity {
             }
         };
 
-
-
         Button buttonOk = findViewById(R.id.buttonOK);
         buttonOk.setOnClickListener((view) -> {
             ReqParam req1 = new ReqParam();
@@ -111,34 +107,30 @@ public class SystemModActivity extends AppCompatActivity {
 
                     //从QUERY_SYSINDEX接口传入修改数据的id值
                     map1.put(ComDef.QUERY_SYSINDEX, id);
-                    //通过SYS_NAME接口修改sysName数据
 
+                    //通过SYS_NAME接口修改sysName数据
                     map1.put(ComDef.SYS_NAME, editTextSysName.getText().toString());
-                    Log.i("cccccccccccc", editTextSysName.getText().toString());
+                    Log.i("修改的系统名称", editTextSysName.getText().toString());
 
                     //通过SYS_NAME接口修改detial数据
-
                     map1.put(ComDef.SYS_DETIAL, editTextSysDetial.getText().toString());
 
                     //通过SYS_NAME接口修改linkman数据
-
                     map1.put(ComDef.SYS_LINKMAN, editTextSysLinkMan.getText().toString());
 
+                    //通过SYS_PHONE接口修改phone数据
                     map1.put(ComDef.SYS_PHONE, s);//获取需要的字段联系人电话
                     req1.setMap(map1);
                     new GetData(req1) {
 
                         @Override
                         public void dealResult(String result) throws JSONException {
-
                             SystemModActivity.this.runOnUiThread(() -> {
-
                                 ToastUtil.toastCenter(SystemModActivity.this, result);
-                                Log.i("ccccccccccccssssthg",result);
+                                Log.i("返回结果result",result);
                             });
-
-
                         }
+
                     };
 
                     finish();
@@ -150,12 +142,7 @@ public class SystemModActivity extends AppCompatActivity {
             }else {
                 Toast.makeText(SystemModActivity.this, "有值为空，请检查！", Toast.LENGTH_LONG).show();
             }
-
-
-            Log.i("cccccccccccc15646984123", editTextSysLinkManPhone.getText().toString());
-
-
-
+            Log.i("获取的电话号码为：", editTextSysLinkManPhone.getText().toString());
 
         });
 
@@ -169,6 +156,7 @@ public class SystemModActivity extends AppCompatActivity {
     }
 
     public static boolean isPhoneNumberValid(String phoneNumber){
+
         boolean isValid=false;
 
         String expression01="^\\(?(\\d{3})\\)?[- ]?(\\d{4})[- ]?(\\d{4})$";
@@ -177,10 +165,23 @@ public class SystemModActivity extends AppCompatActivity {
         Matcher m01=p01.matcher(phoneNumber);//检查电话号码的格式是否正确
         Pattern p02=Pattern.compile(expression02);
         Matcher m02=p02.matcher(phoneNumber);
+
         if(m01.matches()||m02.matches()){
             isValid=true;
         }
+
         return isValid;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == android.R.id.home) {//返回前一页
+            finish();
+        }
+
+        return true;
     }
 
 }
